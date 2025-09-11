d40286deaac0cd6bb2e04a95630111d957dd08b1
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-11 11:55:24 +0300

    Chore: Adding new files

---
+++ src/test/java/com/organic/certification/CertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organic.certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
