c394514476bc3d10e97d1ab443812a475b7d8dcc
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-11 14:55:01 +0300

    Fix: Implement our ApiResponseEntity  Done

--- src/main/java/com/organic/certification/common/response/ApiResponseEntity.java
+++ src/main/java/com/organic/certification/common/response/ApiResponseEntity.java
@@ -1,4 +1,30 @@
 package com.organic.certification.common.response;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public record ApiResponseEntity() {
+    public static ResponseEntity<?> success(String message) {
+        ApiResponse<?> response = new ApiResponse<>(HttpStatus.OK.value(), message);
+        return ResponseEntity.ok(response);
+    }
+
+    public static <T> ResponseEntity<?> success(String message, T data) {
+        if(data instanceof Page<?>) {
+            Page<T> page = (Page<T>) data;
+            PaginatedResponse<?> paginatedResponse = new PaginatedResponse<>(
+                    page.getContent(),
+                    page.getNumber() + 1,
+                    page.getSize(),
+                    page.getTotalElements(),
+                    page.getTotalPages(),
+                    page.isLast()
+            );
+            return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), message,paginatedResponse));
+        }
+        return  ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), message, data));
+
+    }
+
 }
