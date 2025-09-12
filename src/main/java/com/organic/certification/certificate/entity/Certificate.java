cfd6a1396fe1bfe28666f4ae4549bd1b1757d800
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-12 10:37:49 +0300

    Chore: Modeling our Certificate Entity done

--- src/main/java/com/organic/certification/certificate/entity/Certificate.java
+++ src/main/java/com/organic/certification/certificate/entity/Certificate.java
@@ -1,4 +1,29 @@
 package com.organic.certification.certificate.entity;
 
-public class certificate {
+import com.organic.certification.farm.entity.Farm;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "certificates")
+@Data
+public class Certificate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String certificateNo;
+
+    private LocalDate issueDate;
+
+    private LocalDate expiryDate;
+
+    private String pdfUrl;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "farm_id", nullable = false)
+    private Farm farm;
 }
