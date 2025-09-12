ca5ead4122e651962182463a9d3e2570e0afbfe5
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-12 10:31:27 +0300

    Chore: Modeling our Field Entity done

--- src/main/java/com/organic/certification/field/entity/Field.java
+++ src/main/java/com/organic/certification/field/entity/Field.java
@@ -1,4 +1,25 @@
 package com.organic.certification.field.entity;
 
-public class Fields {
+import com.organic.certification.farm.entity.Farm;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "fields")
+@Data
+public class Field {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String name;
+    private String crop;
+    private Double areaHa;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "farm_id", nullable = false)
+    private Farm farm;
 }
