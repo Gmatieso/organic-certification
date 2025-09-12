6ac03cf829ac73abda2e0f688fc7f0af1197ca76
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-12 10:36:13 +0300

    Fix: Modeling our Inspection checklist Done

--- src/main/java/com/organic/certification/inspection_checklist/entity/InspectionChecklist.java
+++ src/main/java/com/organic/certification/inspection_checklist/entity/InspectionChecklist.java
@@ -1,4 +1,24 @@
 package com.organic.certification.inspection_checklist.entity;
 
+import com.organic.certification.inspection.entity.Inspection;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "inspection_checklists")
+@Data
 public class InspectionChecklist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String question;
+
+    private Boolean answer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inspection_id", nullable = false)
+    private Inspection inspection;
 }
