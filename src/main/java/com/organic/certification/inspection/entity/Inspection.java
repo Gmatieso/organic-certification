850c62499d40a64f62c7d3e9666c103960c9ea66
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-12 10:33:19 +0300

    Chore: Modeling our Inspection Entity done

--- src/main/java/com/organic/certification/inspection/entity/Inspection.java
+++ src/main/java/com/organic/certification/inspection/entity/Inspection.java
@@ -1,4 +1,37 @@
 package com.organic.certification.inspection.entity;
 
+import com.organic.certification.common.enums.InspectionEnum;
+import com.organic.certification.farm.entity.Farm;
+import com.organic.certification.inspection_checklist.entity.InspectionChecklist;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "inspections")
+@Data
 public class Inspection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private LocalDate date;
+    private String inspectorName;
+
+    @Enumerated(EnumType.STRING)
+    private InspectionEnum status;
+
+    private Double complianceScore;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "farm_id", nullable = false)
+    private Farm farm;
+
+
+    @OneToMany(mappedBy = "inspection", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<InspectionChecklist> checklist = new ArrayList<>();
 }
