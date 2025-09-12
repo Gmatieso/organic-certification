7c05f9bdf290b074e6e4e55185b2cd72d9539c75
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-12 10:42:30 +0300

    Fix: Modeling our Farmer Entity Done

--- src/main/java/com/organic/certification/farmer/entity/Farmer.java
+++ src/main/java/com/organic/certification/farmer/entity/Farmer.java
@@ -1,4 +1,31 @@
 package com.organic.certification.farmer.entity;
 
+import com.organic.certification.farm.entity.Farm;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.List;
+
+
+@Entity
+@Data
+@Table(name = "farmers")
 public class Farmer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+    private String county;
+
+    @OneToMany(mappedBy = "farmer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Farm> farms = new ArrayList<>();
+
 }
