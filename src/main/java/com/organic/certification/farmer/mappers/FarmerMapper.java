359294cbb0171187bfc08982b0cf3aec06ffd679
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-15 07:48:36 +0300

    Fix: Code refactored FarmerMapper

--- src/main/java/com/organic/certification/farmer/mappers/FarmerMapper.java
+++ src/main/java/com/organic/certification/farmer/mappers/FarmerMapper.java
@@ -1,4 +1,15 @@
 package com.organic.certification.farmer.mappers;
 
+import com.organic.certification.farmer.dtos.FarmerRequest;
+import com.organic.certification.farmer.dtos.FarmerResponse;
+import com.organic.certification.farmer.entity.Farmer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface FarmerMapper {
+   Farmer toEntity(FarmerRequest request);
+
+   @Mapping(target = "id", source ="id")
+  FarmerResponse toResponse(Farmer farmer);
 }
