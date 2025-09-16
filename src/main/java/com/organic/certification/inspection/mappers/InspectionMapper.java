04a160830c9f44b20b6d23a20acb31d2a358b3f4
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-16 14:36:20 +0300

    Feat: Implementing  InspectionMapper done

---
+++ src/main/java/com/organic/certification/inspection/mappers/InspectionMapper.java
@@ -0,0 +1,19 @@
+package com.organic.certification.inspection.mappers;
+
+import com.organic.certification.inspection.dtos.InspectionRequest;
+import com.organic.certification.inspection.dtos.InspectionResponse;
+import com.organic.certification.inspection.entity.Inspection;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface InspectionMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "farm", ignore = true)
+    Inspection toEntity(InspectionRequest inspectionRequest);
+
+    @Mapping(target = "farmResponse", ignore = true)
+    InspectionResponse toResponse(Inspection inspection);
+
+
+}
