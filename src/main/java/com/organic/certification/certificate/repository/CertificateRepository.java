1e6fa3ba55d060ec45245d91e2e699dfe6fd04b6
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-17 02:36:45 +0300

    Fix: Integer to UUID

--- src/main/java/com/organic/certification/certificate/repository/CertificateRepository.java
+++ src/main/java/com/organic/certification/certificate/repository/CertificateRepository.java
@@ -3,5 +3,7 @@ package com.organic.certification.certificate.repository;
 import com.organic.certification.certificate.entity.Certificate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CertificateRepository extends JpaRepository<Certificate, Integer> {
+import java.util.UUID;
+
+public interface CertificateRepository extends JpaRepository<Certificate, UUID> {
 }
