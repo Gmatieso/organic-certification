1b63752524b94ce8e7d62ab35714738a7620c128
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-16 10:38:51 +0300

    Fix: Change interger to UUI to match the Field entity

--- src/main/java/com/organic/certification/field/repository/FieldRepository.java
+++ src/main/java/com/organic/certification/field/repository/FieldRepository.java
@@ -3,5 +3,7 @@ package com.organic.certification.field.repository;
 import com.organic.certification.field.entity.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FieldRepository extends JpaRepository<Field, Integer> {
+import java.util.UUID;
+
+public interface FieldRepository extends JpaRepository<Field, UUID> {
 }
