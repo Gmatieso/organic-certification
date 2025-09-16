c933740212ada650f5f74216195ac11d59fce8f5
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-16 14:44:38 +0300

    Feat: Scarfolding InspectionService done

---
+++ src/main/java/com/organic/certification/inspection/service/InspectionService.java
@@ -0,0 +1,18 @@
+package com.organic.certification.inspection.service;
+
+import com.organic.certification.inspection.dtos.InspectionRequest;
+import com.organic.certification.inspection.dtos.InspectionResponse;
+import com.organic.certification.inspection.entity.Inspection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.UUID;
+
+public interface InspectionService {
+    InspectionResponse  createInspection(InspectionRequest inspectionRequest);
+    InspectionResponse  updateInspection(UUID id, InspectionRequest inspectionRequest);
+    void deleteInspection(UUID id);
+    Page<InspectionResponse> getInspections(PageRequest pageRequest);
+    InspectionResponse  getInspection(UUID id);
+    Inspection getInspectionByIdOrThrow(UUID id);
+}
---
+++ src/main/java/com/organic/certification/inspection/service/InspectionServiceImpl.java
@@ -0,0 +1,4 @@
+package com.organic.certification.inspection.service;
+
+public class InspectionServiceImpl {
+}
