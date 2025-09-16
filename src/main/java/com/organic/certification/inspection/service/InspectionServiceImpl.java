7b5b3d3bf66af1e957b643b620a336ad247535dc
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-16 15:14:43 +0300

    Feat: Implement getInspectionByIdOrThrow method done

--- src/main/java/com/organic/certification/inspection/service/InspectionServiceImpl.java
+++ src/main/java/com/organic/certification/inspection/service/InspectionServiceImpl.java
@@ -1,8 +1,12 @@
 package com.organic.certification.inspection.service;
 
+import com.organic.certification.common.exception.ResourceNotFoundException;
+import com.organic.certification.farm.service.FarmService;
 import com.organic.certification.inspection.dtos.InspectionRequest;
 import com.organic.certification.inspection.dtos.InspectionResponse;
 import com.organic.certification.inspection.entity.Inspection;
+import com.organic.certification.inspection.mappers.InspectionMapper;
+import com.organic.certification.inspection.repository.InspectionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +17,10 @@ import java.util.UUID;
 @Service
 @AllArgsConstructor
 public class InspectionServiceImpl implements InspectionService {
+    private final InspectionRepository inspectionRepository;
+    private final InspectionMapper inspectionMapper;
+    private final FarmService farmService;
+
     @Override
     public InspectionResponse createInspection(InspectionRequest inspectionRequest) {
         return null;
@@ -40,6 +48,7 @@ public class InspectionServiceImpl implements InspectionService {
 
     @Override
     public Inspection getInspectionByIdOrThrow(UUID id) {
-        return null;
+        return inspectionRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Inspection with id" + " " + id + " " + "not found"));
     }
 }
