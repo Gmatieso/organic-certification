7933adcc5b38df871f3753f9755ea5deb4f45e80
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-17 09:52:18 +0300

    Scarfold our CheckListMapper implementation done

---
+++ src/main/java/com/organic/certification/inspection_checklist/mappers/CheckListMapper.java
@@ -0,0 +1,20 @@
+package com.organic.certification.inspection_checklist.mappers;
+
+import com.organic.certification.inspection_checklist.dtos.CheckListRequest;
+import com.organic.certification.inspection_checklist.dtos.CheckListResponse;
+import com.organic.certification.inspection_checklist.entity.InspectionChecklist;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+
+@Mapper(componentModel = "spring")
+public interface CheckListMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "inspection", ignore = true)
+    InspectionChecklist toEntity(CheckListRequest checkListRequest);
+
+    @Mapping(target = "inspectionResponse", ignore = true)
+    CheckListResponse toResponse(InspectionChecklist checklist);
+
+
+}
