4b2b6629bf94ca3657bcbf1800c0cce0870b074a
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-17 10:01:53 +0300

    Fix: Scarfold our CheckListController implementation done

--- src/main/java/com/organic/certification/inspection_checklist/controller/CheckListController.java
+++ src/main/java/com/organic/certification/inspection_checklist/controller/CheckListController.java
@@ -1,4 +1,13 @@
 package com.organic.certification.inspection_checklist.controller;
 
+import com.organic.certification.common.config.ApiConfig;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(CheckListController.PATH)
+@AllArgsConstructor
 public class CheckListController {
+    public static final String PATH = ApiConfig.BASE_API_PATH + "checklists";
 }
