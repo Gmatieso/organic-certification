f2479fcf071e48df51a5d5f94603ff69e9d48729
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-18 13:12:26 +0300

    Add findByInspectionId method to checkListRepository that gives us checklist answers tied to inspection

--- src/main/java/com/organic/certification/inspection_checklist/repository/ChecklistRepository.java
+++ src/main/java/com/organic/certification/inspection_checklist/repository/ChecklistRepository.java
@@ -3,7 +3,9 @@ package com.organic.certification.inspection_checklist.repository;
 import com.organic.certification.inspection_checklist.entity.InspectionChecklist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ChecklistRepository extends JpaRepository<InspectionChecklist, UUID> {
+    List<InspectionChecklist> findByInspectionId(UUID inspectionId);
 }
