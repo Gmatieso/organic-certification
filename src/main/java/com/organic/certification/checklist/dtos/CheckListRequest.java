9626d6290101c534fd429732ffe2ece788a81377
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-18 18:42:29 +0300

    Fix: Updating CheckList answers

--- src/main/java/com/organic/certification/checklist/dtos/CheckListRequest.java
+++ src/main/java/com/organic/certification/checklist/dtos/CheckListRequest.java
@@ -3,11 +3,11 @@ package com.organic.certification.checklist.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.UUID;
+
 
 public record CheckListRequest(
-        @NotBlank(message = "Question is required")
-        String question,
-        @NotNull(message = "Answer must be true or false")
+        UUID checklistId,
         Boolean answer
 ) {
 }
