b7b27a99f98408305fb24524c4c71b7f857c47d7
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-18 23:41:24 +0300

    Fix: alot of stuffs

---
+++ src/main/java/com/organic/certification/checklist/controller/ChecklistController.java
@@ -0,0 +1,4 @@
+package com.organic.certification.checklist.controller;
+
+public class ChecklistController {
+}
---
+++ src/main/java/com/organic/certification/checklist/service/ChecklistService.java
@@ -0,0 +1,4 @@
+package com.organic.certification.checklist.service;
+
+public class CheckListService {
+}
